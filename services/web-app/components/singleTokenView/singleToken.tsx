4969edf5cc5eadd643790b6fa065ca821ff94481
Samkbe <samkbe@gmail.com>
2022-12-20 19:37:53 -0500

    Finished Token info on right side of TokenView page, working on transaction log

--- services/web-app/components/singleTokenView/singleToken.tsx
+++ services/web-app/components/singleTokenView/singleToken.tsx
@@ -1,16 +1,26 @@
 import styled from 'styled-components';
-import type { TokenInfoQuery } from '../../.utils/gql/types/graphql';
+import type { TokenInfoQuery, TokenAttribute } from '../../.utils/gql/types/graphql';
 
 type ImageColumnProps = {
   url: string | null | undefined;
 }
+type ImageInfoPointProps = {
+  label: string;
+  info: string;
+}
+type MetaDataInfoPointProps = {
+  label: string;
+  metaData: TokenAttribute[] | undefined | null;
+}
 
-
+const EventsListWrapper = styled.div`
+  border: 1px solid black;
+  height: 22vh;
+`;
 const SingleTokenViewWrapper = styled.div`
   display: grid;
   grid-template-columns: 50% 50%;
 `;
-
 const ImageColumnWrapper = styled.div`
   display: flex;
   align-items: center;
@@ -19,13 +29,17 @@ const ImageColumnWrapper = styled.div`
   border-right: 1px solid black;
   margin-bottom: 5vh;
 `;
+const ImageInfoWrapper = styled.div`
+  height: 75vh;
+  padding: 0px 10px 10px;
+`;
 
 export const SingleTokenView = ({ token } : TokenInfoQuery) => {
-
+  console.log('TOKEN: ', token);
   return (
     <SingleTokenViewWrapper>
       <ImageColumn url={token?.token.image?.mediaEncoding?.__typename === "ImageEncodingTypes" ? token.token.image.mediaEncoding.large : ''}/>
-      <ImageInfo />
+      <ImageInfo token={token}/>
     </SingleTokenViewWrapper>
   )
 }
@@ -38,9 +52,60 @@ const ImageColumn = ({ url }: ImageColumnProps) => {
   )
 }
 
-const ImageInfo = () => {
+const ImageInfo = ({ token } : TokenInfoQuery) => {
+  return (
+    <ImageInfoWrapper>
+      <ImageInfoPoint label="Title" info={token?.token.name ? token?.token.name : 'N/A'}/>
+      <ImageInfoPoint label="Description" info={token?.token.description ? token?.token.description : 'N/A'}/>
+      <ImageInfoPoint label="Collection" info={token?.token.collectionName ? token?.token.collectionName : 'N/A'}/>
+      <ImageInfoPoint label="Current Owner" info={token?.token.owner ? token?.token.owner : 'N/A'}/>
+      <MetaDataInfoPoint label="MetaData" metaData={token?.token.attributes}/>
+      <EventsList />
+    </ImageInfoWrapper>
+  )
+}
+
+const ImageInfoPoint = ({ label, info }: ImageInfoPointProps) => {
+  return (
+    <p>
+      <b>{label}</b>: {info}
+    </p>
+  )
+}
+
+const MetaDataInfoPoint = ({ label, metaData }: MetaDataInfoPointProps) => {
+  if (metaData) {
+    return (
+      <>
+        <b>{label}:</b><br></br>
+        {metaData?.map((data) => {
+          return (
+            <>
+            <p><b>{data.traitType}:</b> {data.value}</p>
+            </>
+          )
+        })}
+      </>
+    )
+  }
+  return (
+    <></>
+  )
+}
+
+const EventsList = () => {
   return (
     <>
+      <p>Activity:</p>
+      <EventsListWrapper>
+
+      </EventsListWrapper>
     </>
+  )
+}
+
+const EventItem = () => {
+  return (
+    <></>
   )
 }
