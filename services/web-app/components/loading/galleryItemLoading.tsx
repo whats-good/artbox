da1600933c40f91d120ddb3034341ae98009f39c
Samkbe <samkbe@gmail.com>
2022-12-23 09:02:59 -0500

    Added loading component

---
+++ services/web-app/components/loading/galleryItemLoading.tsx
@@ -0,0 +1,14 @@
+import { Actmovie303 } from '@react95/icons'
+import styled from 'styled-components';
+
+const GalleryItemLoadingWrapper = styled.div`
+
+`
+
+export const GalleryItemLoading = () => {
+  return (
+    <GalleryItemLoadingWrapper>
+      <Actmovie303 variant="32x32_4"/>
+    </GalleryItemLoadingWrapper>
+  )
+}
