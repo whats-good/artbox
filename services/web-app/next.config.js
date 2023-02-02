8ae6f0f729735da06e14591d536f141ba64bf0ce
Samkbe <samkbe@gmail.com>
2023-02-02 15:12:21 -0800

    Dependencies added to monorepo

--- services/web-app/next.config.js
+++ services/web-app/next.config.js
@@ -4,4 +4,11 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
