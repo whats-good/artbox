64b40a613be3a2d720d5894c6672c5f1fd12510d
Samkbe <samkbe@gmail.com>
2023-02-08 13:33:01 -0800

    f

--- services/app-server/src/main.ts
+++ services/app-server/src/main.ts
@@ -406,7 +406,7 @@ app.use(
       // secure: process.env.NODE_ENV === 'production',
       secure: false,
       httpOnly: true,
-      sameSite: false,
+      sameSite: 'none',
       maxAge: 6000000,
     },
     store: new PrismaSessionStore(new PrismaClient(), {
