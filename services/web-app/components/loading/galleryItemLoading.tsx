5e1ab14351386e9ccd73b21453542503677986ee
Samkbe <samkbe@gmail.com>
2023-02-22 17:03:03 -0800

    Creted shared styles file for loading component

--- services/web-app/components/loading/galleryItemLoading.tsx
+++ services/web-app/components/loading/galleryItemLoading.tsx
@@ -1,15 +1,7 @@
-import styled from "styled-components";
 import LoadingGif from "../../assets/QR8-loading-large.gif";
 import Image from "next/image";
+import { GalleryItemLoadingWrapper } from "./styles";
 
-const GalleryItemLoadingWrapper = styled.div`
-  margin: auto;
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-`;
 export const PageLoading = () => {
   return (
     <GalleryItemLoadingWrapper>
