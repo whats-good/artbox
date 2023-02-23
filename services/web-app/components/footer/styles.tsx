09ee6ff778710e35cdd9b511d71dd49fd662dc0f
Samkbe <samkbe@gmail.com>
2023-02-22 17:05:19 -0800

    Added shared styles file for footer component

---
+++ services/web-app/components/footer/styles.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+export const FooterWrapper = styled.div`
+  width: 100vw;
+  border-top: 1px solid black;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+export const FooterText = styled.p`
+  padding-left: 1vw;
+`;
+export const LogoImage = styled(Image)`
+  height: 30px;
+  width: auto;
+  padding-left: 5vw;
+`;
